02035d2c4e09982a7e1c21964ef6c5f0d95860ee
Niranjan <niranjanchigulla@gmail.com>
2023-04-15 10:45:13 +0000

    Nxt Slides

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,14 @@
-import React from 'react'
 import './index.css'
 
 const Header = () => (
-  <div className="navbar">
-    <div>
-      <img
-        className="navLogo"
-        src="https://assets.ccbp.in/frontend/react-js/nxt-slides/nxt-slides-logo.png"
-        alt="nxt slides logo"
-      />
-    </div>
-    <h1>Nxt Slides</h1>
+  <div className="header-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-slides/nxt-slides-logo.png"
+      alt="nxt slides logo"
+      className="logo"
+    />
+    <h1 className="title">Nxt Slides</h1>
   </div>
 )
+
 export default Header
